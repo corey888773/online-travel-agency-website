b89d8cd3158da023ff626c0732e41e371a7ae903
Piotr Gąsiorek <piotrgasiorek773@gmail.com>
2024-01-04 21:07:05 +0100

    remove unused search pipe

--- frontend/src/app/trip-search.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TripSearchPipe } from './trip-search.pipe';
-
-describe('TripSearchPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TripSearchPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
